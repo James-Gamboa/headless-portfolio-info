8283a72a04ee07ebdd33b186347113a8c86e9516
James-Gamboa <jjguevarag@gmail.com>
2025-03-21 11:17:59 -0600

    update

--- config/database.js
+++ config/database.js
@@ -1,27 +1,26 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
+const parse = require("pg-connection-string").parse;
 
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+module.exports = ({ env }) => {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("DATABASE_URL environment variable is not set");
+  }
+
+  const config = parse(process.env.DATABASE_URL);
+
+  return {
     connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: env.bool('DATABASE_SSL', true) ? {
-        rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', false),
-      } : false,
-      connectionTimeoutMillis: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+      client: "postgres",
+      connection: {
+        host: config.host,
+        port: config.port,
+        database: config.database,
+        user: config.user,
+        password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+      debug: false,
     },
-    pool: {
-      min: env.int('DATABASE_POOL_MIN', 2),
-      max: env.int('DATABASE_POOL_MAX', 10),
-      idleTimeoutMillis: env.int('DATABASE_IDLE_TIMEOUT', 30000),
-      createTimeoutMillis: env.int('DATABASE_CREATE_TIMEOUT', 30000),
-      acquireTimeoutMillis: env.int('DATABASE_ACQUIRE_TIMEOUT', 30000),
-    },
-    debug: env.bool('DATABASE_DEBUG', false),
-  },
-});
+  };
+};
