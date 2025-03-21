8b920760e61e480166662356a43762095f94b3d4
James-Gamboa <jjguevarag@gmail.com>
2025-03-20 19:44:25 -0600

    update

--- config/middlewares.js
+++ config/middlewares.js
@@ -19,7 +19,7 @@ module.exports = [
     config: {
       enabled: true,
       headers: '*',
-      origin: ['http://localhost:3000', 'https://portfolio-james-git-headless-project-jamesgamboas-projects.vercel.app']
+      origin: ['http://localhost:3000', 'https://*.vercel.app']
     }
   },
   'strapi::poweredBy',
